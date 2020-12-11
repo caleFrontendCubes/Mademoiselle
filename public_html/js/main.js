f1e16ac043d8bf8ff5ad253029c12c7aa04d1905
Stanislava <stanislava.jovanovic87@gmail.com>
2020-12-11 13:24:04 +0100

    Blog page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,7 @@
 
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     //ANIMATION
     function animation() {
         let  windowHeight = $(window).height();
@@ -24,9 +24,39 @@ $(document).ready(function(){
     });
     animation();
 
-    
-    
-    
-    
+    if ($('.blog-items-slider').length > 0) {
+        $(".blog-items-slider").owlCarousel({
+            loop: true,
+            nav: true,
+            navText: [
+                '<span class="fas fa-angle-double-left"></span>',
+                '<span class="fas fa-angle-double-right"></span>'
+            ],
+            responsive: {
+                0: {
+                    items: 1,
+                    slideBy: 1,
+                    margin: 0,
+                    dots:false
+                },
+                700: {
+                    items: 2,
+                    slideBy: 2,
+                    margin: 14,
+                    dots:false
+                },
+                1100: {
+                    items: 3,
+                    slideBy: 3
+                }
+            }
+     
+        });
+    }
+
+
+
+
+
 });
 
